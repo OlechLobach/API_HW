0d3ff57e9b281ad13855b6bb6ff9e0a9673aa8fa
Lobach Oleg Igorovich <oleg.lb@icloud.com>
2024-09-16 14:21:51 +0200

    Add project files.

---
+++ API_HW/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace API_HW
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
